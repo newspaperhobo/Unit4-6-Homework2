5ace246fd22db1b7aa2252433acd869d94411042
newspaperhobo <hoboinabox@gmail.com>
2021-04-03 14:10:52 -0400

    initial commit

--- HW-unit4-6-p2/index.js
+++ HW-unit4-6-p2/index.js
@@ -1,46 +1,83 @@
 //Homework Part One
 
-//Using what you did with Objects perdiod 2 from the slides, create a personal profile. Instead of Products you will need to create a personal profile. Initialize Constructors and More!
+//Using what you did with Objects period 2 from the slides, create a personal profile. Instead of Products you will need to create a personal profile. Initialize Constructors and More!
 //Step 1
-
-
-
+function Person(firstName, lastName, birthYear, age, hobbies) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.age = age;
+    this.hobbies = hobbies;
+}
 
 //Step 2
-
+let person1 = new Person("Kira", "Novak", "1989", 31);
+console.log(person1);
 
 
 
 //Step 3
 
+let person2 = new Person("Renee", "Goudreau", "1989", 31);
+console.log(person2);
+
+let person3 = new Person("Emma", "Soucy", "1993", 27);
+console.log(person3);
+
+let person4 = new Person("Colin", "Meloy", "1974", 46);
+console.log(person4);
 
 
 //Step 4
-
-
+function Hobbies(faveActivity, playsSports) {
+    this.faveActivity = faveActivity;
+    this.playsSports = playsSports;
+}
 //Step 5
+let hobbies1 = new Hobbies("Mushroom Hunting", true);
+let hobbies2 = new Hobbies("Crocheting", false);
+let hobbies3 = new Hobbies("Video games", false)
+let hobbies4 = new Hobbies("Playing guitar", true)
 
+console.log(hobbies1, hobbies2, hobbies3, hobbies4)
 
 //Step 6
+let profile1 = new Person("Kira", "Novak", "1989", 31, hobbies1)
+console.log(profile1)
 
+let profile2 = new Person("Renee", "Goudreau", "1989", 31, hobbies2)
+console.log(profile2)
+
+let profile3 = new Person("Emma", "Soucy", "1993", 27, hobbies3)
+console.log(profile3)
+
+let profile4 = new Person("Colin", "Meloy", "1974", 46, hobbies4)
+console.log(profile4)
+
+console.log(profile4.hobbies.faveActivity);
 
 // Step 7
-
-
-// Step 8
-
+profile1.eatsMeat = false;
+console.log(profile1)
 
 //.Create
 
 //Step 1
-
+let Region = {
+    location: "USA",
+    displayRegion: function() {
+        console.log(this.location)
+    }
+};
 
 //Step 2
-
+let USA = Object.create(Region);
+USA.displayRegion()
 
 //Step 3
-
-
+let Canada = Object.create(Region);
+Canada.location = "Canada";
+Canada.displayRegion();
 
 //Array vs Objects
 
