5ea23a9665e2854549bfe2a478af2c9b35d1ad0f
Amber Hampton <37483133+alhamp@users.noreply.github.com>
2021-04-03 10:05:12 -0400

    Unit 4.6 Homework Template for Students
    Objects and Arrays Personal Profile

---
+++ HW-unit4-6-p2/index.js
@@ -0,0 +1,93 @@
+//Homework Part One
+
+//Using what you did with Objects perdiod 2 from the slides, create a personal profile. Instead of Products you will need to create a personal profile. Initialize Constructors and More!
+//Step 1
+
+
+
+
+//Step 2
+
+
+
+
+//Step 3
+
+
+
+//Step 4
+
+
+//Step 5
+
+
+//Step 6
+
+
+// Step 7
+
+
+// Step 8
+
+
+//.Create
+
+//Step 1
+
+
+//Step 2
+
+
+//Step 3
+
+
+
+//Array vs Objects
+
+//Objects: Create a hobby object with properties with name, descriptions and boolean of a property name of supplies needed.
+
+//Use dot notation to return hobby description
+
+//Use bracket notation to return name
+
+
+//Arrays: Create an array with 4 different hobbies
+
+
+//access second item in the array
+
+//Objects
+//Change the value of the boolean of the object we created.
+
+//Add a new property to our person object such as supplies with multiple values in the property.
+
+//remove the hobby description property from the person object
+
+
+//Arrays
+//Use push to add another hobby to your array of hobbies
+
+
+//use pop to remove the last item from an hobby array
+
+
+//use unshift to add more values to the beginning of the hobby array.
+
+// us shift to remove the item from the hobby of the array
+
+//Object
+//Write a for in loop for the person object you created
+
+//Write a for each loop for your person Object
+
+
+//Arrays
+//Write a for loop for your hobby array
+
+//Write a for of loop for your hobby array
+
+//write a for each loop for your hobby array
+
+
+
+
